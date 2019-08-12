b0cbfd752e0f4bcb0cab446f577f6d94f256ffcd
Zealll <eriznis@gmail.com>
2019-08-12 17:03:53 -0400

    finished MVP and Stretch

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const db = require('./data/dbConfig.js');
 
 const server = express();
 
+const accountRoutes = require('./routers/Router.js')
+
+
 server.use(express.json());
+
+server.use('/api/accounts', accountRoutes)
 
 module.exports = server;
